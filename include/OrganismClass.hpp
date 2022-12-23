1f6414a4ed8814e13cffe41bbe8119568e188b18
aagahoz <ozdemiragah24@gmail.com>
2022-12-23 15:04:00 +0300

    General revisions

--- include/OrganismClass.hpp
+++ include/OrganismClass.hpp
@@ -1,4 +1,31 @@
 #ifndef OrganismClass_hpp
 #define OrganismClass_hpp
 
+#include "SystemClass.hpp"
+
+class Organism
+{
+private:
+    System *systems;
+    int index;
+    int size;
+
+public:
+    Organism();
+    Organism(int size);
+
+    void setIndex(int index);
+    int getIndex();
+
+    void setSize(int size);
+    int getSize();
+
+    void addSystemToOrganism(const System &newSystem);
+
+    void setSystems(System* size);
+    System* getSystems();
+
+    void printSystems();
+};
+
 #endif
--- src/OrganismClass.cpp
+++ src/OrganismClass.cpp
@@ -1,1 +1,52 @@
 #include "OrganismClass.hpp"
+
+Organism::Organism()
+{
+    this->systems = nullptr;
+    index = 0;
+}
+Organism::Organism(int size)
+{
+    this->systems = new System[size];
+    this->size = size;
+    index = 0;
+}
+
+void Organism::setIndex(int index)
+{
+    this->index = index;
+}
+int Organism::getIndex()
+{
+    return this->index;
+}
+
+void Organism::setSize(int size)
+{
+    this->size = size;
+}
+int Organism::getSize()
+{
+    return this->size;
+}
+
+void Organism::addSystemToOrganism(const System &newSystem)
+{
+    if (index < size)
+    {
+        this->systems[index] = newSystem;
+        index++;
+    }
+    else
+    {
+        cout << "Index out of range" << endl;
+    }
+}
+
+void Organism::printSystems()
+{
+    for(int i = 0; i < this->size; i++)
+    {
+        systems[i].print();
+    }
+}
