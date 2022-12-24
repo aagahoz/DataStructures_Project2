c5600c40c5ad14badd3704861e54fe25704f84ee
aagahoz <ozdemiragah24@gmail.com>
2022-12-24 16:33:38 +0300

    There is no mutation but hw finished

--- src/main.cpp
+++ src/main.cpp
@@ -25,98 +25,120 @@ int main()
     const int countOfTissueForSystem = countOfTissueForLimb * countOfLimbForSystem;
 
     ifstream verilerFile;
-    verilerFile.open("DATAs.txt");
+    verilerFile.open("veriler.txt");
     string line;
     int countOfTissue = 0;
+    bool isFileOpen = false;
     while (getline(verilerFile, line))
     {
+        isFileOpen = true;
         countOfTissue++;
     }
     verilerFile.close();
 
-    cout << "Count of Tissue : " << countOfTissue << endl;
-    const int countOfSystem = countOfTissue / countOfTissueForSystem;
-    cout << "Count of System : " << countOfSystem << endl;
-    const int countOfLimb = countOfTissue / countOfTissueForLimb;
-    cout << "Count of Limb : " << countOfLimb << endl;
-
-    Tissue *tissues = new Tissue[countOfTissue];
-    Limb *limbs = new Limb[countOfLimb];
-    System *systems = new System[countOfSystem];
-    Organism *organism = new Organism(countOfSystem);
-
-    int indexTissue = 0;
-    int indexLimbs = 0;
-    int indexSystems = 0;
-
-    verilerFile.open("DATAs.txt");
-    while (getline(verilerFile, line))
+    if (isFileOpen)
     {
-        stringstream s(line);
-        stringstream finderCountOfCells(line);
-        string numberChar;
 
-        int sizeTissue = 0;
-        while (finderCountOfCells >> numberChar)
+        // cout << "Count of Tissue : " << countOfTissue << endl;
+        const int countOfSystem = countOfTissue / countOfTissueForSystem;
+        // cout << "Count of System : " << countOfSystem << endl;
+        const int countOfLimb = countOfTissue / countOfTissueForLimb;
+        // cout << "Count of Limb : " << countOfLimb << endl;
+
+        Tissue *tissues = new Tissue[countOfTissue];
+        Limb *limbs = new Limb[countOfLimb];
+        System *systems = new System[countOfSystem];
+        Organism *organism = new Organism(countOfSystem);
+        // cout << "Flag 1" << endl;
+        int indexTissue = 0;
+        int indexLimbs = 0;
+        int indexSystems = 0;
+
+        verilerFile.open("veriler.txt");
+        // cout << "Flag 1.011" << endl;
+
+        while (getline(verilerFile, line))
         {
-            sizeTissue++;
-        }
+            // cout << "Flag 1.1" << endl;
 
-        tissues[indexTissue].setSize(sizeTissue);
-
-        while (s >> numberChar)
-        {
-            int number = stoi(numberChar);
-            Cell *tempCell = new Cell(number);
-            tissues[indexTissue].add(*tempCell);
-        }
-
-        RadixSort::sort(tissues[indexTissue].getCells(), tissues[indexTissue].getSize());
-
-        indexTissue++;
-    }
-    verilerFile.close();
-
-    for (int i = 0; i < countOfLimb; i++)
-    {
-        for (int j = 0; j < countOfTissueForLimb; j++)
-        {
-            if (((i * countOfTissueForLimb) + j) > countOfTissue)
+            stringstream s(line);
+            stringstream finderCountOfCells(line);
+            string numberChar;
+            // cout << "Flag 2" << endl;
+            int sizeTissue = 0;
+            while (finderCountOfCells >> numberChar)
             {
-                break;
+                sizeTissue++;
             }
-            limbs[i].insertTissue(&tissues[(i * countOfTissueForLimb) + j]);
-        }
-    }
 
-    cout << "Veriler Organa Basari ile eklendi !!!" << endl;
-    cout << "Sira sisteme eklemede !!!" << endl;
+            tissues[indexTissue].setSize(sizeTissue);
 
-    for (int i = 0; i < countOfSystem; i++)
-    {
-        for (int j = 0; j < countOfLimbForSystem; j++)
-        {
-            if (((i * countOfLimbForSystem) + j) > countOfLimb)
+            while (s >> numberChar)
             {
-                break;
+                // cout << "Flag 3" << endl;
+                int number = stoi(numberChar);
+                // cout << "Flag 4" << endl;
+                Cell *tempCell = new Cell(number);
+                tissues[indexTissue].add(*tempCell);
+                // cout << "Flag 5" << endl;
             }
-            systems[i].addLimb(&limbs[(i * countOfLimbForSystem) + j]);
+
+            // cout << "Flag 6" << endl;
+            RadixSort::sort(tissues[indexTissue].getCells(), tissues[indexTissue].getSize());
+            // cout << "Flag 7" << endl;
+
+            indexTissue++;
         }
+        verilerFile.close();
+
+        for (int i = 0; i < countOfLimb; i++)
+        {
+            for (int j = 0; j < countOfTissueForLimb; j++)
+            {
+                if (((i * countOfTissueForLimb) + j) > countOfTissue)
+                {
+                    break;
+                }
+                limbs[i].insertTissue(&tissues[(i * countOfTissueForLimb) + j]);
+            }
+        }
+
+        // cout << "Veriler Organa Basari ile eklendi !!!" << endl;
+        // cout << "Sira sisteme eklemede !!!" << endl;
+
+        for (int i = 0; i < countOfSystem; i++)
+        {
+            for (int j = 0; j < countOfLimbForSystem; j++)
+            {
+                if (((i * countOfLimbForSystem) + j) > countOfLimb)
+                {
+                    break;
+                }
+                systems[i].addLimb(&limbs[(i * countOfLimbForSystem) + j]);
+            }
+        }
+
+        // cout << "Veriler Sistem Sinifina Basari ile eklendi !!!" << endl;
+
+        for (int i = 0; i < countOfSystem; i++)
+        {
+            organism->addSystemToOrganism(systems[i]);
+        }
+        // cout << "Veriler Organismaya Sinifina Basari ile eklendi !!!" << endl;
+
+        // organism->printSystems();
+        cout << endl;
+        cout << endl;
+        organism->makeBalanceCheck();
+        organism->printBalanced();
+
+        cout << endl;
+        cout << endl;
     }
-
-    cout << "Veriler Sistem Sinifina Basari ile eklendi !!!" << endl;
-
-    for (int i = 0; i < countOfSystem; i++)
+    else
     {
-        organism->addSystemToOrganism(systems[i]);
+        cout << "Dosya Okuma Sorunu" << endl;
     }
-    cout << "Veriler Organismaya Sinifina Basari ile eklendi !!!" << endl;
-
-    // organism->printSystems();
-    cout << endl;
-    cout << endl;
-    organism->makeBalanceCheck();
-    organism->printBalanced();
 
     return 0;
 }
